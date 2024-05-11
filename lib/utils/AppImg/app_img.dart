a7f3e0f74bdc61e483189c83fc2475ca02e45c02
rafsanopi <rafsan3@dti.ac>
2024-05-11 17:52:27 +0600

    Image added

--- lib/utils/AppImg/app_img.dart
+++ lib/utils/AppImg/app_img.dart
@@ -1,5 +1,36 @@
 class AppImages {
   static const String basePath = "assets/images";
 
-  static const String logo = "$basePath/logo.png";
+  ///====================== OnBoarding Image =========================
+
+  static const String onBoarding1 = "$basePath/onBoarding1.png";
+  static const String onBoarding2 = "$basePath/onBoarding2.png";
+  static const String onBoarding3 = "$basePath/onBoarding3.png";
+
+  ///====================== Banner Image =========================
+  //TODO Have to remove those image
+  static const String banner1 = "$basePath/banner1.png";
+  static const String banner2 = "$basePath/banner2.png";
+
+  ///====================== Home Image =========================
+
+  static const String dentalCondition = "$basePath/dentalCondition.png";
+  static const String skinCare = "$basePath/skinCare.png";
+
+  //TODO Have to remove those image
+  static const String currentOffer1 = "$basePath/currentOffer1.png";
+  static const String currentOffer2 = "$basePath/currentOffer2.png";
+  static const String currentOffer3 = "$basePath/currentOffer3.png";
+
+  ///====================== Sub Category =========================
+
+  //TODO Have to remove those image
+  static const String dental1 = "$basePath/dental1.png";
+  static const String dental2 = "$basePath/dental2.png";
+  static const String dental3 = "$basePath/dental3.png";
+  static const String skin1 = "$basePath/skin1.png";
+  static const String skin2 = "$basePath/skin2.png";
+  static const String skin3 = "$basePath/skin3.png";
+
+  static const String hygienist = "$basePath/Hygienist.png";
 }
