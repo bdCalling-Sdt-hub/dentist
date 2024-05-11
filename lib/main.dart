5ee27d1408c19d2b18300e97261474747f584ded
rafsanopi <rafsan3@dti.ac>
2024-05-11 16:40:10 +0600

    Added Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:dentist/core/app_routes/app_routes.dart';
 import 'package:dentist/core/dependency/dependency.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,60 +16,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: GetMaterialApp(
+        //theme: ThemeData(useMaterial3: false),
+        debugShowCheckedModeBanner: false,
+        defaultTransition: Transition.fadeIn,
+        transitionDuration: const Duration(milliseconds: 200),
+        initialRoute: AppRoute.splashScreen,
+        navigatorKey: Get.key,
+        getPages: AppRoute.routes,
       ),
     );
   }
--- lib/utils/AppColors/app_colors.dart
+++ lib/utils/AppColors/app_colors.dart
@@ -1,5 +1,46 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color whiteColor = Color(0xffffffff);
+  ///======================== Blue Series ===========================
+
+  static const Color blue50 = Color(0xffe7ebed);
+  static const Color blue100 = Color(0xffb6c0c8);
+  static const Color blue200 = Color(0xff92a2ae);
+  static const Color blue300 = Color(0xff607888);
+  static const Color blue400 = Color(0xff415d71);
+  static const Color blue500 = Color(0xff12354e);
+  static const Color blue600 = Color(0xff103047);
+  static const Color blue700 = Color(0xff0d2637);
+  static const Color blue800 = Color(0xff0a1d2b);
+  static const Color blue900 = Color(0xff081621);
+
+  ///======================== Blue Series ===========================
+
+  static const Color additional = Color(0xffE2BCC1);
+
+  ///======================== Loght Series ===========================
+
+  static const Color loght50 = Color(0xffffffff);
+  static const Color loght100 = Color(0xfffefefe);
+  static const Color loght200 = Color(0xfffefefe);
+  static const Color loght300 = Color(0xfffdfdfd);
+  static const Color loght400 = Color(0xfffdfdfd);
+  static const Color loght500 = Color(0xfffcfcfc);
+  static const Color loght600 = Color(0xffe5e5e5);
+  static const Color loght700 = Color(0xffb3b3b3);
+  static const Color loght800 = Color(0xff8b8b8b);
+  static const Color loght900 = Color(0xff6a6a6a);
+
+  ///======================== Dark Series ===========================
+
+  static const Color dark50 = Color(0xfff0f0f0);
+  static const Color dark100 = Color(0xffd1d1d1);
+  static const Color dark200 = Color(0xffbababa);
+  static const Color dark300 = Color(0xff9b9b9b);
+  static const Color dark400 = Color(0xff888888);
+  static const Color dark500 = Color(0xff6a6a6a);
+  static const Color dark600 = Color(0xff606060);
+  static const Color dark700 = Color(0xff4b4b4b);
+  static const Color dark800 = Color(0xff3a3a3a);
+  static const Color dark900 = Color(0xff2d2d2d);
 }
