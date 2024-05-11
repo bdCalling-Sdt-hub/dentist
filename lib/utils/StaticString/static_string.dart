ac0a435f3b8cee79ab14b6e7bec7d2d335ac079f
rafsanopi <rafsan3@dti.ac>
2024-05-11 17:27:09 +0600

    Static Strings

--- lib/utils/StaticString/static_string.dart
+++ lib/utils/StaticString/static_string.dart
@@ -1,4 +1,65 @@
 class AppStaticStrings {
-  //=============================OnBoarding================================
-  static const accessSpecializedCare = "Access Specialized";
+  //============================= OnBoarding ================================
+  static const dental = "Dental";
+  static const banningSmileClub =
+      "Banning Smile Club an exclusive service provided by banning dental group ltd. copyright 2024";
+  static const getStarted = "Get Started";
+  static const welcomeBack = "Welcome Back !";
+  static const pleaseEnterYour = "Please Enter Your provided informations ";
+  static const email = "Email";
+  static const enterYourEmail = "Enter your email here";
+  static const pinNo = "Pin no";
+  static const enterYourPinNum = "Enter your pin number";
+  static const password = "Password";
+  static const enterYourPassword = "Enter your Password";
+  static const logIn = "Log in";
+
+  //============================= Home Screen ================================
+
+  static const searchhere = "Search here";
+  static const expertsAdvice = "Experts’ Advice";
+  static const dentalCondition = "Dental Condition";
+  static const skinCondition = "Skin Condition";
+  static const fAQ = "FAQ";
+  static const seeMore = "See more";
+  static const currentOffers = "Current Offers";
+  static const smartCheck = "Smart Check";
+
+  static const home = "Home";
+  static const myPlan = "My Plan";
+  static const profile = "Profile";
+  static const needHelp = "Need Help";
+
+  static const dentalApp = "Dental app";
+  static const message = "Message";
+
+  ///==================== Side Drawer ======================
+
+  static const myProfile = "My Profile";
+  static const settings = "Settings";
+  static const aboutUs = "About us";
+  static const privacyPolicy = "Privacy Policy";
+  static const termsConditions = "Terms & Conditions";
+  static const logOut = "Log Out";
+  static const changePassword = "Change Password";
+  static const oldPassword = "Old Password";
+  static const newPassword = "New Password";
+  static const confirmPassword = "Confirm Password";
+
+  ///========================== Profiles ===========================
+  static const userProfile = "User Profile";
+  static const name = "Name";
+  static const contactNo = "Contact No";
+  static const dateofbirth = "Date of birthe";
+  static const age = "Age";
+  static const gender = "Gender";
+  static const plan = "Plan";
+  static const editUserProfile = "Edit User Profile";
+  static const saveChange = "Save & Change";
+
+  ///========================== Others ===========================
+
+  static const aboutus = "About us";
+  static const notifications = "Notifications";
+  static const search = "Search";
 }
