f4df7f3a244bc5287ed3e5f922219945d2d9082e
rafsanopi <rafsan3@dti.ac>
2024-05-12 13:56:39 +0600

    Added Icons

--- lib/utils/AppIcons/app_icons.dart
+++ lib/utils/AppIcons/app_icons.dart
@@ -1,5 +1,23 @@
 class AppIcons {
   static const String basePath = "assets/icons";
 
-  static const String archive = "$basePath/archive.svg";
+  static const String aboutUs = "$basePath/aboutUs.svg";
+  static const String changePass = "$basePath/aboutUs.svg";
+  static const String edit = "$basePath/aboutUs.svg";
+  static const String faq = "$basePath/aboutUs.svg";
+
+  static const String homeUnselected = "$basePath/home_unselected.svg";
+  static const String homeSelected = "$basePath/homeSelected.svg";
+  static const String logOut = "$basePath/logOut.svg";
+  static const String myPlanSelected = "$basePath/myPlanSelected.svg";
+  static const String myPlanUnselected = "$basePath/myPlanUnselected.svg";
+
+  static const String needHelpSelected = "$basePath/needHelpSelected.svg";
+  static const String needHelpUnselected = "$basePath/needHelpUnselected.svg";
+
+  static const String privacyPolicy = "$basePath/privacyPolicy.svg";
+  static const String profileSelected = "$basePath/profileSelected.svg";
+  static const String profileUnselected = "$basePath/profileUnselected.svg";
+  static const String settings = "$basePath/settings.svg";
+  static const String terms = "$basePath/terms.svg";
 }
